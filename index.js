bb4f912000ee1ea0200640e356712c06ef578c18
Marco Whyte <mwhyte@umass.edu>
2020-03-12 14:51:07 -0500

    horoku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
-const PORT = 5000;
 
 const server = require("./server");
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
